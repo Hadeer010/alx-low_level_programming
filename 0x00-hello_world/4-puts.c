86c0da8e89303b211b254add4770d92c5123358e
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-10 15:52:20 -0700

    first pro in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry and main function
+ *
+ * Return: Always 0 (No Errors )
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
