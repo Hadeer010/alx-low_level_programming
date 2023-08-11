31e6e2195c84ca30d2b456628b981b05fb64b1d1
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-11 16:07:10 -0700

    first commit in else if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point function
+ *
+ * Return: Always 0 (no errors)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative", n);
+	} else
+	{
+		printf("%d n is zero", n);
+	}
+
+
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this is readme for else if and loops 
