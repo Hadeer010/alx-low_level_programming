b90f2189d21121565fc31edb6e864cb80f52b258
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-12 08:53:02 -0700

    advanced 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point function
+ *
+ * Return: Always 0 (no errors)
+ */
+int main(void)
+{
+int i, j;
+
+i = 0;
+
+while (i < 10)
+{
+j = i + 1;
+
+while (j < 10)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+j++;
+}
+i++;
+}
+return (0);
+}
