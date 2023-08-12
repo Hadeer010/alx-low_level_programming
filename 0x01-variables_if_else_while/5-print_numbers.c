b9c40d81ab9f65cab4216d0653b7c401494aced5
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-11 19:53:17 -0700

    f

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point function
+ *
+ * Return: Always 0 (no errors)
+ */
+int main(void)
+{
+int i;
+for (i = 0; 1 <= 10; 1++)
+{
+printf("%d", i)
+}
+return (0);
+}
