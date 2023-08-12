2b0a47fa6f5f400e22a1080b6c802100ea364c27
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-12 06:29:01 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,16 @@ int main(void)
 {
 	int i = 0;
 
-	do{
-		putchar(i);
-		putchar(',');
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		i++;
-	} while (i < 10);
+	}
+	putchar('\n');
 	return (0);
 }
