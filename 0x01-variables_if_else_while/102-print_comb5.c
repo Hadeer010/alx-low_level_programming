65c48029018fa2927d14a9e8f16a01276cd33454
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-12 10:14:27 -0700

    exist

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point function
+ *
+ * Return: Always 0 (no errors)
+ */
+int main(void)
+{
+int i, j;
+
+i = 0;
+
+while (i < 100)
+{
+j = i + 1;
+
+while (j < 100)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
