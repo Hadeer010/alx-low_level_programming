c03f4b0a61dad5dece7d7d55cf6a0cedfd0b340f
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-11 17:53:54 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point function
+ *
+ * Return: Always 0 (no errors)
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	do {
+		putchar(alphabet);
+		alphabet++;
+	} while (alphabet <= 'z');
+
+
+	return (0);
+}
