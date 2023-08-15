e02ad2a9ba0d23cfaac9feb684d374559bb0a938
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-15 16:33:39 -0700

    s

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,11 @@
  *_isalpha - shows if the letter
  * is in alphacase or not
  * @c: is the character of ASCII Code
- * Retur: 1 for alpha and . 0 for lower
+ * Return: 1 for alpha and . 0 for lower
  */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
 	{
 		return (0);
 	}
