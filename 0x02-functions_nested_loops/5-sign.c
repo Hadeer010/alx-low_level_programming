986586194b6dcf2530e4f47c23275342b57ea179
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-15 17:15:51 -0700

    git

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - shows if te number type
+ * if it's greater, small or equal to zero
+ * @n: the number
+ * Return: 1 if the num is greater than zero,
+ * -1 is less than zero
+ * and 0 is zero
+ */
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
