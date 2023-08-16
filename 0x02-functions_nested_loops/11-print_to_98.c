103b2d963851f0d5b06f80c93356f6023d36e6d4
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-15 18:58:39 -0700

    klk

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,14 @@ void print_to_98(int n)
 	{
 		for (c = n; c >= 98; c--)
 		{
-			printf("%d. ", c);
+			printf("%d, ", c);
 		}
 	}
 	else
 	{
 		for (c = n; c <  98; c++)
 		{
-			printf("%d. ", c);
+			printf("%d, ", c);
 		}
 		printf("98\n");
 	}
