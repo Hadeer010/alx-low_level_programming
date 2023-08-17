5c7bc67f2a2758857c46511a7287fa88673c1304
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-17 14:41:57 -0700

    k

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _isdigit - hecks for a digit (0 through 9).
- * @cl: input
+ * @c: input
  * Return: 1 if successful c is a digit, 0 if isn't a digit
  */
 int _isdigit(int c)
 {
-	int cl;
-	if (cl >= 48 & cl <= 57)
+	if (c >= 48 & c <= 57)
 	{
+		_putchar(c);
 		return (1);
 	}
 	else
