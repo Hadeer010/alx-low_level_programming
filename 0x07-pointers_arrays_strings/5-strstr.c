748cc0240ca150594d9247b2452a763227f91508
Hadeer010 <hadeerelgaml0@gmail.com>
2023-08-28 17:11:34 -0700

    dsf

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,31 @@
 #include "main.h"
+/**
+ * _strstr - ocates a substring.
+ * @hocates: string
+ * @needle:  first occurrence of the substring
+ * Return: substring in c
+ */
 char *_strstr(char *haystack, char *needle)
 {
+	if (*needle == '\0')
+	{
+		return haystack;
+	}
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return haystack;
+		}
+		haystack++;
+	}
+	return (0);
 }
