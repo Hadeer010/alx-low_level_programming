75eaf3341e8cc2cab32f739198d33b9579241914
Hadeer010 <hadeerelgaml0@gmail.com>
2023-09-01 14:35:16 -0700

    fs

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints program name
+ * @argc: integer
+ * @argv: interger
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
