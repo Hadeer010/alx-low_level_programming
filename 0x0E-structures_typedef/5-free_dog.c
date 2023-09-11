452f6f7b1331627e22a54c52f16d40a61fb869c8
Hadeer010 <hadeerelgaml0@gmail.com>
2023-09-11 14:33:11 -0700

    kjhbg

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dogs
+ * @d: yo dog
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
