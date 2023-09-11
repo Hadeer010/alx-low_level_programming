5160951478415a54c7503307886f1fbff273f766
Hadeer010 <hadeerelgaml0@gmail.com>
2023-09-11 12:29:54 -0700

    f

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - a dog struct
  * @name: dog's name
  * @age: dog's age
- * @owner: dog's owner
+ * @owner: dog's owner name
  */
 struct dog
 {
-	char name;
-	float age;
-	char owner;
+	char *name;
+	float *age;
+	char *owner;
 };
 #endif
