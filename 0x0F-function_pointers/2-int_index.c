95bbcbd83d66740474bfbe7b30cd5603822cac8e
Hadeer010 <hadeerelgaml0@gmail.com>
2023-09-13 19:13:56 -0700

    kjhg

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index – search for integer
+ * @array: the int arry
+ * @size: the array size
+ * @cmp: function of compare
+ * Return: integer
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x = 0;
+	if (array && size && cmp)
+	{
+		while (x < size)
+		{
+			if (cmp(array[x]))
+			{
+				return(x);
+			}
+			x++;
+		}
+	}
+	return (-1);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+/**
+ * op_add – add intergers
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub – sub intergers
+ * @a: first int
+ * @b: second int
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul – mul intergers
+ * @a: first int
+ * @b: second int
+ * Return: mul
+ */
+int op_ mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div – div intergers
+ * @a: first int
+ * @b: second int
+ * Return: div
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+
+/**
+ * op_div – mod intergers
+ * @a: first int
+ * @b: second int
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
