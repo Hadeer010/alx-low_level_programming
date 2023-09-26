28d76611b3e22d8c6f8cfd15d05bba272f0f2ca9
Hadeer010 <hadeerelgaml0@gmail.com>
2023-09-26 16:53:13 -0700

    9 again

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int x; 
+	unsigned int x;
 	listint_t *new;
 	listint_t *temp = *head;
 
@@ -36,8 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp->next = new;
 			return (new);
 		}
-		else
-			temp = temp->next;
+
 	}
 
 	return (NULL);
